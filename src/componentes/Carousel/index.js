d4d8b59722eeead047b0668fe934c56327b4d0ce
Lucas Novae Dragojevic <lucas.dragojevic@gmail.com>
2023-03-01 15:55:54 -0300

    Animated carousel

--- src/componentes/Carousel/index.js
+++ src/componentes/Carousel/index.js
@@ -1,20 +1,43 @@
+import React, { useEffect, useRef, useState } from 'react'
 import { View, FlatList, Image } from 'react-native'
 import styles from './styles'
 
 export default function Carousel({data}) {
-    return <View style={styles.container}>
-        <FlatList
-              data={data}
-              horizontal
-              showsHorizontalScrollIndicator={false}
-              keyExtractor={item => item.id}
-              renderItem={({ item }) => (
-                <Image
-                  source={item.imagem}
-                  style={styles.image}
-                  resizeMode="contain"
-                />
-              )}
-            />
-    </View>
+
+  const carouselRef = useRef()
+  const [myIndex, setMyIndex] = useState(0)
+
+  function changeVectorPosition() {
+    if (myIndex < data.length - 1)
+      setMyIndex(myIndex++)
+    else
+      setMyIndex(0)
+  }
+
+  useEffect(() => {
+    carouselRef.current.scrollToIndex({index:myIndex})
+    const interval = setInterval(() => {
+      changeVectorPosition()
+    }, 2000)
+    return () => clearInterval(interval)
+  }, [myIndex])
+
+  return <View style={styles.container}>
+      <FlatList
+            data={data}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            keyExtractor={item => item.id}
+            renderItem={({item, index}) => (
+              <Image
+                source={item.imagem}
+                style={[styles.image,
+                index == data.length - 1 ? {marginRight: 200} : null
+                ]}
+                resizeMode="contain"
+              />
+            )}
+            ref={carouselRef}
+          />
+  </View>
 }
